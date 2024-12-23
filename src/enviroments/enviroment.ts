069441ebbb4de1a4d2c7b0f78752c2317691af2c
Malcolm Abdullah <mabdullah4562@gmail.com>
2024-12-22 23:34:27 -0600

    Auto-generated commit

--- src/enviroments/enviroment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://portfolio-2etr.onrender.com'  // Your web service URL for development
-};
--- src/enviroments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://portfolio-2etr.onrender.com'  // Web service URL for production
-};
